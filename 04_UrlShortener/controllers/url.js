77143f5d106735ac3a17c140cb87b04dc6cb427d
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-06-09 13:50:00 +0530

    backend

---
+++ 04_UrlShortener/controllers/url.js
@@ -0,0 +1,27 @@
+const shortid = require("shortid");
+const URL = require("../models/url.model");
+
+async function handleGenerateNewShortURl(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "URL is required." });
+  const shortId = shortid();
+  await URL.create({
+    shortId,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortId });
+}
+
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
+module.exports = { handleGenerateNewShortURl,
+  handleGetAnalytics
+ };
--- 04_UrlShortener/index.js
+++ 04_UrlShortener/index.js
@@ -1,8 +1,30 @@
-const express = require('express')
+const express = require("express");
+const urlRoute = require("./routes/url");
+const URL = require('./models/url.model')
+const { connectMongo } = require("./connection");
+const app = express();
+const PORT = 3001;
 
-const app = express()
-const PORT = 3001
+connectMongo("mongodb://127.0.0.1:27017/short-url")
+  .then(() => console.log("Mongo Connected"))
+  .catch((err) => console.log("Error : MongoDB : ", err));
 
-app.listen(PORT, ()=>{
-    console.log(`Server started at PORT:${PORT}`);
+app.use(express.json())
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId',async(req,res)=>{
+  const shortId = req.params.shortId
+  const entry = await URL.findOneAndUpdate({
+    shortId
+  },{$push:{
+    visitHistory : {timestamp : Date.now()},
+  }})
+  res.redirect(entry.redirectURL)
 })
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started at PORT:${PORT}`);
+});
