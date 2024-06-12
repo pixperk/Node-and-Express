a70565446c93e9499e09d7029e97767fbe3a5b11
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-06-12 19:54:34 +0530

    complete auth

--- 04_UrlShortener/controllers/url.js
+++ 04_UrlShortener/controllers/url.js
@@ -9,6 +9,7 @@ async function handleGenerateNewShortURl(req, res) {
     shortId,
     redirectURL: body.url,
     visitHistory: [],
+    createdBy : req.user._id
   });
   return res.render('home',{
     id: shortId
--- 04_UrlShortener/index.js
+++ 04_UrlShortener/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require('cookie-parser')
+const {restrictToLoggedInUserOnly, checkAuth} = require('./middleware/auth')
+
 const urlRoute = require("./routes/url");
-const staticRoute = require('./routes/staticRouter')
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+
 const URL = require("./models/url.model");
 const { connectMongo } = require("./connection");
+
 const app = express();
 const PORT = 3001;
 
@@ -15,17 +21,20 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
+
 
 app.get("/test", async (req, res) => {
   const allUrls = await URL.find({});
-  return res.render('home',{
-    urls : allUrls,
+  return res.render("home", {
+    urls: allUrls,
   });
 });
 
-app.use("/url", urlRoute);
-app.use('/',staticRoute)
+app.use("/url", restrictToLoggedInUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/",checkAuth, staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
